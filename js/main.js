3140d1c5a63853dc57f9d12b97bae558e2db6b66
corm0078 <corm0078@algonquinlive.com>
2014-09-30 11:08:17 -0400

    changing to make both work

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var sales = $('.sales');
 
 
 var salesClickHandler = function() {
-    var isActive = sales.attr('data-state');
+    var isActive = salesDetails.attr('data-state');
     
     if (isActive == 'active') {
         salesDetails.attr('data-state','inactive');
@@ -14,4 +14,23 @@ var salesClickHandler = function() {
 };
 
 
-salesDetails.on('click', salesClickHandler);
+sales.on('click', salesClickHandler);
+
+
+var infoDetails1 = $('.info__details1');
+var info1 = $('.info1');
+
+
+var sales1ClickHandler = function() {
+    var isActive = infoDetails1.attr('data-state');
+    
+    if (isActive == 'active') {
+        infoDetails1.attr('data-state','inactive');
+    
+    }else {    
+  infoDetails1.attr('data-state','active');
+    }
+};
+
+
+info1.on('click', salesClickHandler);
